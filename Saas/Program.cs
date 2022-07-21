baafb209b142636649d7c3691573c2c70c4c24bb
MatrixSensei <dominic.vella@optusnet.com.au>
2022-07-22 08:14:21 +1000

    Show Swagger

--- Saas/Program.cs
+++ Saas/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Saas;
 using Saas.Data;
 using TenantResources.Data;
@@ -7,6 +8,21 @@ using TenantResources.Data.Repository;
 using TenantResources.Data.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
+var addSwagger = true;
+//var addSwagger = false;
+if (addSwagger == true)
+{
+    // Requires NuGet Swashbuckle.AspNetCore package installed
+    // Also add using Microsoft.OpenApi.Models;
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    //builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Saas.Api", Version = "v1" });
+    });
+}
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
@@ -35,6 +51,26 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() && addSwagger == true)
+{
+    app.UseMigrationsEndPoint();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -58,7 +94,7 @@ app.UseEndpoints(endpoints =>
       pattern: "{area=Public}/{controller=Home}/{action=Index}/{id?}"
     );
     endpoints.MapRazorPages(); //Routes for pages
-//    endpoints.MapControllers(); //Routes for my API controllers
+    endpoints.MapControllers(); //Routes for my API controllers
 });
 
 
